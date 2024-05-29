f18cac8471b4a13132e24a980c106f7d31909466
Ruan Lima <az.88004607@gmail.com>
2024-05-29 00:10:48 -0300

    Criacao do componente para o Header lateral

---
+++ src/components/HeaderLateralComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="header column" style="align-items: center; justify-content: center">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b3/Eletrobras.png">
+    </section>
+</template>
+
+<script>
+export default {
+    name: "HeaderLateralComponent",
+}
+
+</script>
+
+<style scoped>
+.header {
+    height: 100vh !important;
+    width: 350px;
+    background-color: #6BC9DB;
+}
+
+.column {
+    display: flex;
+    flex-direction: column;
+}
+
+img {
+    width: 80%;
+    margin-bottom: 180px;
+}
+</style>
